8ba9c5cc791eb8f73254f64c56952400bc5dcb26
Jacob <jcmuffin18@gmail.com>
2024-12-18 13:46:54 +0800

    disabled

--- app.py
+++ app.py
@@ -57,27 +57,27 @@ with st.sidebar:
 st.title("Breast Cancer DSS predictor")
 
 # genetic data
-st.markdown("## Genetic Expression Data")
-# st.write(patient['ESR1','PGR','ERBB2','MKI67','PLAU','ELAVL1','EGFR','BTRC','FBXO6','SHMT2','KRAS','SRPK2','YWHAQ','PDHA1','EWSR1','ZDHHC17','ENO1','DBN1','PLK1','GSK3B'])
+# st.markdown("## Genetic Expression Data")
+# # st.write(patient['ESR1','PGR','ERBB2','MKI67','PLAU','ELAVL1','EGFR','BTRC','FBXO6','SHMT2','KRAS','SRPK2','YWHAQ','PDHA1','EWSR1','ZDHHC17','ENO1','DBN1','PLK1','GSK3B'])
 
 # clinical data
 st.markdown("## Clinical Data")
-age_input = st.number_input("Age:", value=patient['Age'])
-menopausal_state_input = st.toggle("Menopausal State", value=patient['Menopausal State'])
+age_input = st.number_input("Age:", value=patient['Age'], disabled=True)
+menopausal_state_input = st.toggle("Menopausal State", value=patient['Menopausal State'], disabled=True)
 
 st.markdown("### Tumour data")
-tumour_size_input = st.number_input("Enter tumour size in cm:", key="tumour_size", value=patient['Size'])
-neoplasm_histologic_grade_input = st.number_input("Neoplasm Histologic Grade", key="neoplasm", value=patient['Neoplasm Histologic Grade'])
-cellularity_input = st.number_input("Cellularity", value=patient['Cellularity'])
+tumour_size_input = st.number_input("Enter tumour size in cm:", key="tumour_size", value=patient['Size'], disabled=True)
+neoplasm_histologic_grade_input = st.number_input("Neoplasm Histologic Grade", key="neoplasm", value=patient['Neoplasm Histologic Grade'], disabled=True)
+cellularity_input = st.number_input("Cellularity", value=patient['Cellularity'], disabled=True)
 
 st.markdown("### Therapies")
-radio_therapy_input = st.toggle("Radio therapy", value=patient['Radio Therapy'])
-chemotherapy_input = st.toggle("Chemotherapy", value=patient['Chemotherapy'])
-hormone_therapy_input = st.toggle("Hormone Therapy", value=patient['Hormone Therapy'])
+radio_therapy_input = st.toggle("Radio therapy", value=patient['Radio Therapy'], disabled=True)
+chemotherapy_input = st.toggle("Chemotherapy", value=patient['Chemotherapy'], disabled=True)
+hormone_therapy_input = st.toggle("Hormone Therapy", value=patient['Hormone Therapy'], disabled=True)
 
 st.markdown("### Surgeries")
-surgery_breast_conserving_input = st.toggle("Breast conserving surgery", value=patient['Surgery-breast conserving'])
-surgery_mastectomy_input = st.toggle("Masectomy", value=patient['Surgery-mastectomy'])
+surgery_breast_conserving_input = st.toggle("Breast conserving surgery", value=patient['Surgery-breast conserving'], disabled=True)
+surgery_mastectomy_input = st.toggle("Masectomy", value=patient['Surgery-mastectomy'], disabled=True)
 
 st.pyplot(plot_surv_curve(index))
 # st.write(f"Actual label: {patient['Label']}")
