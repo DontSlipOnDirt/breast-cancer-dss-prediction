7966dbbcc9194ba55a3cb60d5b82765bc6cf60d9
Jacob <jcmuffin18@gmail.com>
2024-12-18 12:37:41 +0800

    changes for demo app

--- app.py
+++ app.py
@@ -1,67 +1,69 @@
 import streamlit as st
 import pandas as pd
+import random
+import matplotlib.pyplot as plt
 
 # read in data to display
 data = pd.read_csv("data/test_data.csv")
+predicted_label = pd.read_csv("survival_labels.csv")
+index = 0
 
-# generate UI depending on selection
-def input_page(generate=False):
-    return
+# plot survival curve
+survival_probabilities = pd.read_csv("survival_probabilities.csv")
+
+if 'Patient_ID' in survival_probabilities.columns:
+    df = survival_probabilities.drop(columns=['Patient_ID'])
+survival_probabilities_dict = {col: df[col].values for col in df.columns}
+
+time_points = ['1', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
+
+def plot_surv_curve(index):
+    # Create a new figure and Axes for the subplot
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Extract survival probabilities for the patient at the given index
+    patient_probabilities = [survival_probabilities_dict[t][index] for t in time_points]
+
+    # Plot on the subplot
+    ax.plot(time_points, patient_probabilities, marker='o', label="Survival Probability")
+    ax.axvline(x='60', color='red', linestyle='--', label='x = 60')
+    ax.set_title("Survival Curve for Patient in Test Dataset")
+    ax.set_xlabel("Months")
+    ax.set_ylabel("Survival Score")
+    ax.set_ylim(0, 1.05)
+    ax.grid(True)
+    ax.legend()
+
+    return fig
 
 # generate report
 def generate_report():
-
-    report = f"""
-    # You are going to die in 2 days :)
-    """
-    st.pyplot()
-
-    return report
+    
+    st.pyplot(plot_surv_curve(index))
 
 # sidebar
 with st.sidebar:
     select_case = st.radio(
         "Choose input",
-        ("Example 1", "Example 2", "Random Patient", "Custom")
+        ("Example 1", "Example 2", "Random Patient")
     )
 
 # change page depending on sidebar input
 if select_case == "Example 1 (not survivor)":
-    index = 0
+    index = 15
+    patient = data.iloc[index]
+
 elif select_case == "Example 2 (survivor)":
     index = 1
+
 elif select_case == "Random Patient":
-    index = 2
-elif select_case == "Custom":
-    index = 3
+    index = random.randint(0, 117)
 
 st.title("Breast Cancer DSS predictor")
 
 # genetic data
 st.markdown("## Genetic Expression Data")
 st.text_input("Enter comma-separated expression values")
-
-# ESR1 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key="ESR1")
-# PGR = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=0)
-# ERBB2 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=1)
-# MKI67 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=2)
-# PLAU = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=3)
-# ELAVL1 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=4)
-# EGFR = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=5)
-# BTRC = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=6)
-# FBXO6 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=7)
-# SHMT2 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=8)
-# KRAS = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=9)
-# SRPK2 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=10)
-# YWHAQ = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=11)
-# PDHA1 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=12)
-# EWSR1 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=13)
-# ZDHHC17 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=14)
-# ENO1 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=15)
-# DBN1 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=16)
-# PLK1 = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=17)
-# GSK3B = st.number_input("Enter expression value:", min_value=0.0, max_value=1.0, step=0.1, key=18)
-
 
 # clinical data
 st.markdown("## Clinical Data")
@@ -85,7 +87,6 @@ surgery_mastectomy_input = st.toggle("Masectomy")
 # Generate report button
 if st.button("Generate Report"):
     if age_input > 0:
-        report = generate_report()
-        st.markdown(report)
+        generate_report()
     else:
         st.error("Please fill out the age field.")
